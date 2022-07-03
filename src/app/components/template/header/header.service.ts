5222527231944004db6e2a7123b408c9792ea1b1
Gausshj <kylin_0@qq.com>
2022-07-03 16:11:27 +0800

    Update

---
+++ src/app/components/template/header/header.service.ts
@@ -0,0 +1,25 @@
+import { HeaderData } from './header-data.model';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: '开始',
+    icon: 'home',
+    routeUrl: ''
+  })
+
+  constructor() { }
+
+  get headerData(): HeaderData {
+    return this._headerData.value
+  }
+
+  set headerData(headerData: HeaderData) {
+    this._headerData.next(headerData)
+  }
+}
