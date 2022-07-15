9b1150f3917d0f62cdad742a5e76286c53b9a2ca
Gausshj <kylin_0@qq.com>
2022-07-15 09:44:12 +0800

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,11 +27,9 @@ import { ProductReadComponent } from './components/product/product-read/product-
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { RedDirective } from './directives/red.directive';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from  '@angular/common';
-import { ForDirective } from './directives/for.directive';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { IntroductionComponent } from './views/introduction/introduction.component';
@@ -48,8 +46,6 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    RedDirective,
-    ForDirective,
     ProductUpdateComponent,
     ProductDeleteComponent,
     IntroductionComponent,
